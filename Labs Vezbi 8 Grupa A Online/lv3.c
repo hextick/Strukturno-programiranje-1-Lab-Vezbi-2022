a55183ec0b5294707545b476e82230e00d1b3ce9
hextick <119020866+hextick@users.noreply.github.com>
2022-12-22 21:01:54 +0100

    Update lv3.c

--- Labs Vezbi 8 Grupa A Online/lv3.c
+++ Labs Vezbi 8 Grupa A Online/lv3.c
@@ -3,24 +3,18 @@
 //Да се прочита број N од тастатура. Да се дефинира функција form(int n) што ќе испечати форма од ѕвезди како што е прикажано на сликата.
 //За да се освојат сите поени од вежбата, потребно е функцијата form да биде рекурзивна.
 
-void pecati(int n){
-    for(int i =0;i<n;i++){
-        for(int j=n;j>0;j--){
+void pecati(int num){
+    if(num){
+        for(int i = 0; i<n; i++){
             printf("*");
         }
         printf("\n");
+        pecati(num-1);
     }
-}
-void pecatiRekurzivna(int n){
-    for(int i=0;i<n;i++){
-        printf("*");
-    }
-    printf("\n");
-    pecatiRekurzivna(n-1);
 }
 int main() {
-    int n;
-    scanf("%d",&n);
-    pecatiRekurzivna(n);
+    int N;
+    scanf("%d",&N);
+    pecati(N);
     return 0;
 }
